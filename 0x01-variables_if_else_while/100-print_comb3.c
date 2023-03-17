847881a469302f720fcdcdead71b9b3e0107352f
smartraysam <raysamtob@gmail.com>
2023-03-17 22:54:34 +0100

    Print all possible different combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,11 +16,11 @@ int main(void)
 
 	for (i = 48; i < 57; i++)
 	{
-		for (j = i+1; j <= 57; j++)
+		for (j = i + 1; j <= 57; j++)
 		{
 			putchar(i);
 			putchar(j);
-			if ( i == 56 && j == 57)
+			if (i == 56 && j == 57)
 				continue;
 			putchar(',');
 			putchar(' ');
