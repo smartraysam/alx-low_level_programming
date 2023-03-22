784267bad48c2b7001d130da8131ae88a203c275
smartraysam <raysamtob@gmail.com>
2023-03-22 20:19:35 +0100

    Comment out the infinite loop

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int larget_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
 
 #endif /* MAIN_H */
