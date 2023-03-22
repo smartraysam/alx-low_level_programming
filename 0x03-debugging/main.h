9ac8f24ae0eba62649e47a80d73e7d48d9425f30
smartraysam <raysamtob@gmail.com>
2023-03-22 20:46:37 +0100

    Fix largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int larget_number(int, int, int);
+int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_day(int, int);
 
