e0058b8cc65497bc01cfbd67cc61b6696a88b272
smartraysam <raysamtob@gmail.com>
2023-03-27 15:08:05 +0100

    Function that takes a pointer and update it value to 98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef ALX_H_INCLUDED                                                                                                            
+#define ALX_H_INCLUDED      
+
+void reset_to_98(int *n);
+
+#endif 
