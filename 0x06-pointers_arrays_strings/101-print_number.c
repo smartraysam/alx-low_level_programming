6e697bcea01cd1831d04ff0d0094699c24a3a8b2
smartraysam <raysamtob@gmail.com>
2023-03-30 22:31:44 +0100

    function that prints a buffer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  *print_number - prints an integer
  *@n: integer to print
@@ -9,13 +8,13 @@
 
 void print_number(int n)
 {
-	unsigned int u = n; 
+	unsigned int u = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		u = -u;
 	}
-
 	if (u / 10)
 		print_number(u / 10);
 	_putchar((u % 10) + '0');
