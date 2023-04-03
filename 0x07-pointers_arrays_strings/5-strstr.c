529afaef8c98241dd87e2b2c4a25d659dbb51777
smartraysam <raysamtob@gmail.com>
2023-04-03 16:19:47 +0100

    5=strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - Function searches a substring
+ * @haystack: Parameter string to check
+ * @needle: Parameter accept
+ * Return: a pointer to the beginnning of the located substring
+ * or Null if no such byte is found
+ */
+char  *_strstr(char *haystack, char *needle)
+{
+	int i,  j;
+
+	while (haystack[i] != '\0')
+	{
+		while (haystack[i + j] == needle[j])
+		{
+			j++;
+		}
+
+		if (needle[j] == '\0')
+			return (&hystack[j]);
+		i++;
+	}
+	return (0);
+}
+
