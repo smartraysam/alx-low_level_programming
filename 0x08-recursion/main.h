3b1c1185318f0ee2512b67e1e2e1388111493b65
smartraysam <raysamtob@gmail.com>
2023-04-05 19:55:31 +0100

    0-puts_recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sq_root(int n, int root);
+int _sqrt_recursion(int n);
+int _prime_number(int n, int divisor);
+int is_prime_number(int n);
+int _str_cmp_recursion(char *first, char *last);
+int is_palindrome(char *s);
+int _str_match(char *s1, char *s2, char *after_wldcd);
+int wildcmp(char *s1, char *s2);
+
+#endif
