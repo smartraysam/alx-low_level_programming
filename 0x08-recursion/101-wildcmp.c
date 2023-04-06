a25f446057d31d06f43b4768aafe7aeff0383a3c
smartraysam <raysamtob@gmail.com>
2023-04-06 08:38:12 +0100

    101-wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * _str_match - check if a substring after wildcard matches s1
+ * @s1: one string
+ * @s2: one string
+ * @after_wldcd: placeholder for position right after wildcard
+ * Return: 1 if matched, 0 if not
+ */
+
+int _str_match(char *s1, char *s2, char *after_wldcd)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == '\0' && *s2 == '*')
+	{
+		return (_str_match(s1, s2 + 1, s2 + 1));
+	}
+	if (*s1 == '\0' && *s2 != '\0')
+	{
+		return (0);
+	}
+	if (*s2 == '*')
+	{
+		return (_str_match(s1, s2 + 1, s2 + 1));
+	}
+	if (*s1 == *s2)
+	{
+		return (_str_match(s1 + 1, s2 + 1, after_wldcd));
+	}
+	else
+	{
+		return (_str_match(s1 + 1, after_wldcd, after_wldcd));
+	}
+}
+
+
+
+/**
+ * wildcmp - compare string with wildcard matches
+ * @s1: the string
+ * @s2: the wildcard
+ * Return: 1 if matched, 0 if not
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	else if (*s2 == '*')
+	{
+		return (_str_match(s1, (s2 + 1), (s2 + 1)));
+	}
+	else
+	{
+		return (0);
+	}
+}
