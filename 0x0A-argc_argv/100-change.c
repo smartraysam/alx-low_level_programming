c6706119af777cb6314479b528ff7234533cf4a7
smartraysam <raysamtob@gmail.com>
2023-04-09 19:13:37 +0100

    100-chnage.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - program that prints the minimum number of coins 
+ * main - program that prints the minimum number of coins
  * to make chnage for an amount of money
  * @argc: argument counter
  * @argv: argument vecto
  *
- * Return: always 1
+ * Return: coins
  */
 
 int main(int argc, char *argv[])
@@ -15,35 +15,31 @@ int main(int argc, char *argv[])
 	int n = 0;
 	int coins = 0;
 
-	if (argc == 2)
-	{
-		n = atoi(argv[1]);
-		if (argv[1][0] == 45)
-		{
-			printf("0\n");
-			return (0);
-		}
-		
-		coins += n / 25;
-
-		n = n % 25;
-		coins += n / 10;
-
-		n = n % 10;
-		coins += n / 5;
-
-		n = n % 5;
-		coins += n / 2;
-
-		n = n % 2;
-		coins += n / 1;
-
-		printf("%d\n", coins);
-	}
-	else
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	if (argv[1][0] == 45)
+	{
+		printf("0\n");
+		return (0);
+	}
+	n = atoi(argv[1]);
+	coins += n / 25;
+
+	n = n % 25;
+	coins += n / 10;
+
+	n = n % 10;
+	coins += n / 5;
+
+	n = n % 5;
+	coins += n / 2;
+
+	n = n % 2;
+	coins += n / 1;
+
+	printf("%d\n", coins);
 	return (0);
 }
