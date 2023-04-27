ae94e29463e2809fe0e4ebb26769415d97ad3ded
Smart  Raysam <engsamtob@yahoo.com>
2023-04-27 23:30:57 +0100

    solve task 100 and 101

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,15 +16,12 @@ size_t print_list(const list_t *h)
     while (h != NULL)
     {
         if (h->str == NULL)
-        {
             printf("[0] (nil)\n");
-        }
         else
-        {
-            printf("[%d] %s\n",h->len, h->str);
-        }
+            printf("[%d] %s\n", h->len, h->str);
+        
         h = h->next;
         node_cnt++;
     }
-    return node_cnt;
+    return (node_cnt);
 }
