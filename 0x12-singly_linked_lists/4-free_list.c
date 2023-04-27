5c2de5f93c0ce8431e07397f90a9ba702a08d672
smartraysam <raysamtob@gmail.com>
2023-04-28 00:07:47 +0100

    fix 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -12,13 +12,13 @@
 
 void free_list(list_t *head)
 {
-    list_t *tmp_node;
+	list_t *tmp_node;
 
-    while (head != NULL)
-    {
-        tmp_node = head->next;
-        free(head->str);
-        free(head);
-        head = tmp_node;
-    }
+	while (head != NULL)
+	{
+		tmp_node = head->next;
+		free(head->str);
+		free(head);
+		head = tmp_node;
+	}
 }
