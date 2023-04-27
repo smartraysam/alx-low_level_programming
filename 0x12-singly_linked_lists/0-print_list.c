924951bebeeb573beae4076ae38e13f9c923fd4c
smartraysam <raysamtob@gmail.com>
2023-04-28 00:44:56 +0100

    fix task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,9 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t node_cnt  = 0;
+	unsigned int node_cnt;
+
+	node_cnt = 0;
 
 	while (h != NULL)
 	{
@@ -19,7 +21,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%i] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		node_cnt++;
