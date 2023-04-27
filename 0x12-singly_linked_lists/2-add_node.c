a8e706f2e85a8da96ffba611557fc03f979bea24
smartraysam <raysamtob@gmail.com>
2023-04-27 23:50:59 +0100

    fix task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -24,22 +24,20 @@ int _strlen(const char *str)
  * add_node - add node to begining of list
  * @head: first node
  * @str: new node string
- *
  * Return: address of new element/ NULL if failed
  */
 
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_list = malloc(sizeof(list_t));
+	list_t *new_list = malloc(sizeof(list_t));
 
-    if (new_list == NULL)
-    {
-        return (NULL);
-    }
-    new_list->str = strdup(str);
-    new_list->len = _strlen(str);
-    new_list->next = *head;
-    *head = new_list;
-    return (new_list);
+	if (new_list == NULL)
+		return (NULL);
+
+	new_list->str = strdup(str);
+	new_list->len = _strlen(str);
+	new_list->next = *head;
+	*head = new_list;
+	return (new_list);
 }
