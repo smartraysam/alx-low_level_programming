dbb3b3f4a43875f0e1ddeb3c3ec160c99d0fb41b
smartraysam <raysamtob@gmail.com>
2023-04-27 23:46:47 +0100

    fix task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,18 +5,18 @@
 /**
  * list_len - number of element in  list
  * @h: h - list parameter
- *
  * Description: return number of element in the list
+ * Return: no. of elements
  */
 
 size_t list_len(const list_t *h)
 {
-    size_t node_cnt = 0;
+	size_t node_cnt = 0;
 
-    while (h != NULL)
-    {
-        h = h->next;
-        node_cnt++;
-    }
-    return node_cnt;
+	while (h != NULL)
+	{
+		h = h->next;
+		node_cnt++;
+	}
+	return (node_cnt);
 }
