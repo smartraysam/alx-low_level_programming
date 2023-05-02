308b91425e7e636155b51e32e7e5e8275140497a
smartraysam <raysamtob@gmail.com>
2023-05-02 21:40:47 +0100

    update 3-add node to the end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,31 +1,33 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - add node to the end of list
+ * add_nodeint_end - add new node to end of list
  * @head: first node
- * @n: new node int value
+ * @n: no. pf nodes
  *
- * Return: address of new element/ NULL if failed
+ * Return: new_node or NULL
  */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new_node, *tmp_node;
+	listint_t *new_node, *tmp_node;
 
-    new_node = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
-
-    new_node->n = n;
-    new_node->next = NULL;
-    tmp_node = head;
-    while (tmp_node->next != NULL)
-    {
-        tmp_node = tmp_node->next;
-    }
-    tmp_node->next = new_node;
-    return (new_node);
-}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		tmp_node = *head;
+		while ((tmp_node->next) != NULL)
+		{
+			tmp_node = tmp_node->next;
+		}
+		tmp_node->next = new_node;
+	}
+	return (new_node);
+}}
