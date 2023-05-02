1eb10024bd5313ed1bed9019a1b7f081dbd1660a
smartraysam <raysamtob@gmail.com>
2023-05-02 21:11:33 +0100

    update 0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,16 +7,15 @@
  */
 size_t print_listint(const listint_t *h);
 {
-    unsigned int node_cnt;
+	unsigned int node_cnt;
+	
+	node_cnt = 0;
 
-    node_cnt = 0;
-
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        node_cnt++;
-    }
-
-    return (node_cnt);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node_cnt++;
+	}
+	return (node_cnt);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,9 +16,9 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
+	int n;
+	struct listint_s *next;
+}listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -35,6 +35,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
 
 #endif
