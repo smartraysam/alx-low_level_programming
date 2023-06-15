896a7cc2d95662c2740535a539a5ceb08c796ac0
smartraysam <raysamtob@gmail.com>
2023-06-15 21:19:35 +0100

    Task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -10,11 +10,11 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp_node;
 
-	if(head != NULL)
+	if (head != NULL)
 		while (head->prev != NULL)
 			head = head->prev;
-	
-	while((tmp_node = head) != NULL)
+
+	while ((tmp_node = head) != NULL)
 	{
 		head = head->next;
 		free(tmp_node);
